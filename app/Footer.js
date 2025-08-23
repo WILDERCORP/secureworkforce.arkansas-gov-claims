7a5a83b03605a48e359aab3b340ab84a640e0a23
WILDERCORP <beeowensby850@gmail.com>
2025-08-23 18:33:29 +0100

    new fix

--- app/Footer.js
+++ app/Footer.js
@@ -1,4 +1,3 @@
-import '../../src/styles/globals.css';
 
 export default function Footer() {
   return (
--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import '../../src/styles/globals.css';
 import MainContent from './MainContent';
 import Footer from './Footer';
 
